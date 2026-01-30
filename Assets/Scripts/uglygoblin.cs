d79a93633abad7365373373e5d1ad7263d20faf9
gladryan <ryandewar2@gmail.com>
2026-01-30 23:52:26 +0000

    SLIME JUMPS

---
+++ Assets/Scripts/uglygoblin.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+
+public class uglygoblin : MonoBehaviour
+{
+    Vector3 startpos;
+    private float speed = 3f;
+    private float jumpAmount = 200;
+    private float gravityScale = 1.5f;
+    private int counter;
+    private bool isGrounded;
+    public Transform player;
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startpos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 displacement = player.position - transform.position;
+        displacement = displacement.normalized;
+        if (Vector2.Distance(player.position, transform.position) > 1.0f)
+        {
+            transform.position += (displacement * speed * Time.deltaTime);
+
+        }
+        Vector2 distJump = new Vector2(displacement.x, displacement.y);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Force);
+            isGrounded = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            transform.position = startpos;
+        }
+
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
