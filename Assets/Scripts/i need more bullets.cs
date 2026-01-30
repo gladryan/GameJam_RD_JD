b0bdb2d7695af7d25fd8089a32d5f60f264a2110
gladryan <ryandewar2@gmail.com>
2026-01-30 21:24:47 +0000

    fixed bullet

--- Assets/Scripts/i need more bullets.cs
+++ Assets/Scripts/i need more bullets.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class ineedmorebullets : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    private float prevDist;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        prevDist = 0;
     }
 
     // Update is called once per frame
@@ -13,8 +15,16 @@ public class ineedmorebullets : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Shoot();
+            if (transform.position.x < prevDist)
+            {
+                backShoot();
+            }
+            else
+            {
+                Shoot();
+            }
         }
+        prevDist = transform.position.x;
     }
 
     void Shoot()
@@ -23,7 +33,19 @@ public class ineedmorebullets : MonoBehaviour
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         if (rb != null)
         {
-            rb.AddForce(new Vector2(25,0), ForceMode2D.Impulse);
+            bullet.transform.position += new Vector3(1, 0, 0);
+            rb.AddForce(new Vector2(25, 0), ForceMode2D.Impulse);
+        }
+    }
+
+    void backShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            bullet.transform.position += new Vector3(-1, 0, 0);
+            rb.AddForce(new Vector2(-25, 0), ForceMode2D.Impulse);
         }
     }
 }
